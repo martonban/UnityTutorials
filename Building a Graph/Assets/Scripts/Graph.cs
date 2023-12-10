8f78b6fdc70e7ee16889b1eaf3da38fb38392948
Márton Bán <30813844+martonban@users.noreply.github.com>
2023-12-10 14:47:30 +0100

    Building a Graph  - wip

---
+++ Building a Graph/Assets/Scripts/Graph.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+
+    [SerializeField] private Transform pointPrefab;
+    [SerializeField, Range(10, 100)] private int resolution;
+
+    private void Awake()
+    {
+	    float step = 2f / resolution;
+	    Vector3 position = Vector3.zero;
+	    Vector3 scale = Vector3.one * step;
+		for(int i = 0; i < resolution; i++) {
+		    Transform point = Instantiate(pointPrefab);
+		    position.x = (i + 0.5f) * step - 1f;
+		    position.y = position.x * position.x;
+		    point.localPosition = position;
+			point.localScale = scale;
+			point.SetParent(transform, false);
+	    }
+    }
+}
