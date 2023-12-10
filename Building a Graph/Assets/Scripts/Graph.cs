46a6a666f4a923214468dc89194269cbab070347
Márton Bán <30813844+martonban@users.noreply.github.com>
2023-12-10 19:21:27 +0100

    Building a Graph - DONE
    Implementing URP and add animation

--- Building a Graph/Assets/Scripts/Graph.cs
+++ Building a Graph/Assets/Scripts/Graph.cs
@@ -9,18 +9,31 @@ public class Graph : MonoBehaviour
     [SerializeField] private Transform pointPrefab;
     [SerializeField, Range(10, 100)] private int resolution;
 
+    private Transform[] points;
+
     private void Awake()
     {
 	    float step = 2f / resolution;
 	    Vector3 position = Vector3.zero;
 	    Vector3 scale = Vector3.one * step;
-		for(int i = 0; i < resolution; i++) {
-		    Transform point = Instantiate(pointPrefab);
+	    points = new Transform[resolution];
+		for(int i = 0; i < points.Length; i++) {
+			Transform point = points[i] = Instantiate(pointPrefab);
 		    position.x = (i + 0.5f) * step - 1f;
 		    position.y = position.x * position.x * position.x;
 		    point.localPosition = position;
 			point.localScale = scale;
 			point.SetParent(transform, false);
+	    }
+    }
+
+    private void Update()
+    {
+	    for (int i = 0; i < points.Length; i++) {
+		    Transform point = points[i];
+		    Vector3 position = point.localPosition;
+		    position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
+		    point.localPosition = position;
 	    }
     }
 }
