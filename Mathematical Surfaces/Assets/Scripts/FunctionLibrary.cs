a457264e0d43c5916cf704e38b03e39d3fbe00ec
Márton Bán <30813844+martonban@users.noreply.github.com>
2023-12-11 15:25:03 +0100

    Morphing wave

--- Mathematical Surfaces/Assets/Scripts/FunctionLibrary.cs
+++ Mathematical Surfaces/Assets/Scripts/FunctionLibrary.cs
@@ -9,7 +9,7 @@ public static class FunctionLibary
     
     public static float MultiWave (float x, float t)
     {
-        float y = Sin(PI * (x + t));
+        float y = Sin(PI * (x + 0.5f * t));
         y += 0.5f * Sin(2f * PI * (x + t));
         return y * (2f / 3f);
     }
