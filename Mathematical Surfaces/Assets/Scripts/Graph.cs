587f9fe3d5a20a177cc7b317e538f1c092f3d9b7
Márton Bán <30813844+martonban@users.noreply.github.com>
2023-12-13 16:13:10 +0100

    Mathematical Surfaces - Done

--- Mathematical Surfaces/Assets/Scripts/Graph.cs
+++ Mathematical Surfaces/Assets/Scripts/Graph.cs
@@ -16,19 +16,10 @@ public class Graph : MonoBehaviour
     private void Awake()
     {
 	    float step = 2f / resolution;
-	    Vector3 position = Vector3.zero;
 	    Vector3 scale = Vector3.one * step;
 	    points = new Transform[resolution * resolution];
-		for(int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
-			if (x == resolution) {
-				x = 0;
-				z += 1;
-			}
+		for(int i = 0; i < points.Length; i++) {
 			Transform point = points[i] = Instantiate(pointPrefab);
-		    position.x = (x + 0.5f) * step - 1f;
-		    position.y = position.x * position.x * position.x;
-		    position.z = (z + 0.5f) * step - 1f;
-		    point.localPosition = position;
 			point.localScale = scale;
 			point.SetParent(transform, false);
 	    }
@@ -38,11 +29,18 @@ public class Graph : MonoBehaviour
     {
 	    FunctionLibary.Function f = FunctionLibary.GetFunction(function);
 	    float time = Time.time;
-	    for (int i = 0; i < points.Length; i++) {
-		    Transform point = points[i];
-		    Vector3 position = point.localPosition;
-		    position.y = f(position.x, position.z, time);
-		    point.localPosition = position;
+	    float step = 2f / resolution;
+	    float v = 0.5f * step - 1.0f;
+	    for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
+		    if (x == resolution)
+		    {
+			    x = 0;
+			    z += 1;
+			    v = (z + 0.5f) * step - 1.0f;
+		    }
+
+		    float u = (x + .5f) * step - 1f;
+		    points[i].localPosition = f(u, v, time);
 	    }
     }
 }
